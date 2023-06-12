e84c7fa51900927afcc8d09b9fdf58d400d8adbc
Alexander Malukhin <keeonenew@yandex.ru>
2023-06-12 23:15:19 +0300

    feat: test js code

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,22 @@
 const formGroup = document.querySelector(".newsletter_group");
+const submitButton = document.querySelectorAll(".newsletter__button");
+const formEmail = addEmail;
+const input = formEmail.email;
 
-function formSuccess {
-    
+function formSuccess() {
+    // go
 }
 
 function emailValidate(email) {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailRegex.test(email);
 }
+
+submitButton.addEventListener("click", (evt) => {
+    evt.preventDefault();
+    const email = input.value.trim();
+
+    if(emailValidate()) {
+        formSuccess()
+    }
+})
