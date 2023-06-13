02790a6e91e2238319050bacc1ae5cff28be7a12
Alexander Malukhin <keeonenew@yandex.ru>
2023-06-13 12:13:54 +0300

    feat: try to release

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,26 @@
-const formGroup = document.querySelector(".newsletter_group");
-const submitButton = document.querySelectorAll(".newsletter__button");
-const formEmail = addEmail;
-const input = formEmail.email;
+// const formEmail = document.forms.addEmail;
+// const input = formEmail.email;
+
+
+
+// основное окно слева
+const formGroup = document.querySelector("newsletter_group");
+// картинка справа
+const mainImg = document.querySelector("newsletter__img");
+// Кнопка сохранить email
+const submitButton = document.querySelectorAll("newsletter__button");
+// Инпут для заполнения email
+const emailInput = document.querySelector("newsletter__input");
+// Окно уапешного добавления email
+const emailSuccess = document.querySelector("newslatter__group-thanks");
+// Кнопка закрыть окнно успешного добавления email
+const closeButtonDissmiss = document.querySelector("newsletter__button-dismiss")
+
 
 function formSuccess() {
-    // go
+    emailSuccess.classList.add("newsletter_email-active")
+    formGroup.style.display = "none";
+    mainImg.style.display = "none";
 }
 
 function emailValidate(email) {
@@ -14,9 +30,14 @@ function emailValidate(email) {
 
 submitButton.addEventListener("click", (evt) => {
     evt.preventDefault();
-    const email = input.value.trim();
+    const email = emailInput.value.trim();
 
-    if(emailValidate()) {
+    if (emailValidate()) {
         formSuccess()
     }
+})
+
+closeButtonDissmiss.addEventListener("click", () => {
+    formGroup.style.display = "block";
+    mainImg.style.display = "block";
 })
